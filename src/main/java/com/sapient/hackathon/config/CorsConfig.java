bd95d06dfdf7b79fe932a23fa959c23cdd344b3e
hemmetal <hemant1027@gmail.com>
2023-03-25 17:56:09 +0530

    added cors filter

--- src/main/java/com/sapient/hackathon/config/CorsConfig.java
+++ src/main/java/com/sapient/hackathon/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.sapient.hackathon.filter.config;
+package com.sapient.hackathon.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
