fafae96662925149e63f64b190cab6f0f783e8e4
iqbmohmm <Sapient@121>
2023-03-26 01:38:31 +0530

    change file routing

--- src/main/java/com/sapient/hackathon/controller/FileController.java
+++ src/main/java/com/sapient/hackathon/controller/FileController.java
@@ -13,12 +13,14 @@ import com.sapient.hackathon.repository.SafetyConcernRepository;
 import com.sapient.hackathon.utils.FileUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.List;
 
 @RestController
+@RequestMapping("/file")
 public class FileController {
 
     @Autowired
